8ed6ce0a7d104bdde8c318075bcb07c5b0e0f14b
yasindutharnga <ypremathilaka@corporate.mediadefined.com>
2024-12-30 17:00:48 +0530

    Add project files.

---
+++ UnitTestingDemo.Test.Unit/CalculatorServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingDemo.Test.Unit
+{
+    public class CalculatorServiceTest
+    {
+
+        [Fact]
+        public void ShouldInputTwoIntsAndAddThemReturnInt()
+        {
+
+            //given
+            var calcService = new CalculatorService();
+            var randomIntA = 2;
+            var randomIntB = 5;
+            var expectedValue = 7;
+
+            //when
+            var actualValue = calcService.AddtwoInts(randomIntA, randomIntB);
+
+            //then
+            Assert.Equal(expectedValue, actualValue);
+
+        }
+
+        [Fact]
+        public void ShouldInputTwoDoublesAndAddThemAndReturnDouble()
+        {
+
+            //given
+            var calcService = new CalculatorService();
+            var randomDoubleA = 2.8;
+            var randomDoubleB = 5.7;
+            var expectedValue = 8.5;
+
+            //when
+            var actualValue = calcService.AddtwoDoubles(randomDoubleA, randomDoubleB);
+
+            //then
+            Assert.Equal(expectedValue, actualValue,1);
+
+        }
+    }
+}
---
+++ UnitTestingDemo/CalculatorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingDemo
+{
+    public class CalculatorService
+    {
+        public int AddtwoInts(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double AddtwoDoubles(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
